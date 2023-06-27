0de0dee6b3f9ade9a2fed3653ebb2c9a476b058d
PRAVIN BOKARE <pravinbokare7@gmail.com>
2023-06-27 22:34:21 +0530

    Your commit message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is a Machine Learning End to End project with Machine Learning Operations.',
+    author='pravin-bokare',
+    license='MIT',
+)
